9112445e4f03fdb92954dc3b2751237385500fdc
Laura Santos <lauragsantos7@gmail.com>
2022-08-30 13:20:28 -0300

    exercicios

---
+++ index4-3.js
@@ -0,0 +1,31 @@
+//Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
+let fatorial = 1;
+let resultado = [];
+for (let index = 10; index > 0; index-=1){
+    fatorial *=index
+}
+ console.log(fatorial)
+ // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab".
+ let word = 'macarrao';
+ let reverseWord = ''
+ for (let ind = 0; ind < word.length; ind+=1){
+    reverseWord += word[word.length - 1 - ind]
+ } console.log(reverseWord)
+
+ //Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+ let array = ['java', 'javascript', 'python', 'html', 'css'];
+ for (let indexx=0; indexx < array.length; indexx +=1){
+    console.log(array[indexx])
+ }
+
+
+
+
+ //4- Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
+ let primo ='';
+ let menor = 2;
+ for (let ind = 2; ind < 50; ind+=1){
+    
+ }
+
+   
